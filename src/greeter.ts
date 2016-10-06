35c1faf13a1d99ddbd24475479183cfdda998975
Lody Borgers <hi@lodybo.nl>
2016-10-06 17:23:07 +0200

    Everything runs

--- src/greeter.ts
+++ src/greeter.ts
@@ -2,6 +2,6 @@ function greeter(person: string) {
     return "Hello mr. " + person;
 }
 
-var user = [0,1,2];
+var user = "John Doe";
 
-document.body.innerHTML = greeter();
+document.body.innerHTML = greeter(user);
